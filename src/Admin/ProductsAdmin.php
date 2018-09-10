c255fd8dcd6efb580c8aeb2e3bda35088596e0b5
OlegFedorenko <oefrdorenko87@gmail.com>
2018-09-10 21:12:41 +0300

    lesson 17 админка

---
+++ src/Admin/ProductsAdmin.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oleg
+ * Date: 10.09.18
+ * Time: 20:39
+ */
+
+namespace App\Admin;
+
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class ProductsAdmin extends AbstractAdmin
+{
+    protected function configureListFields(ListMapper $list)
+    {
+        $list
+            ->addIdentifier('id')
+            ->addIdentifier('name')
+            ->addIdentifier('price','currency',['currency'=>'UAH'])
+            ->add('isTop', null, ['editable' => true])
+            ->add('category')
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+            ->add('id')
+            ->add('name')
+            ->add('price')
+            ->add('isTop')
+            ->add('category')
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('isTop')
+            ->add('category')
+        ;
+    }
+
+}
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -113,4 +113,9 @@ class Product
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
 }
