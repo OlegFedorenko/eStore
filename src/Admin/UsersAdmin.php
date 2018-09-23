df0c34aaef072c2d7a949d0e481496ed8d903909
OlegFedorenko <oefrdorenko87@gmail.com>
2018-09-23 14:48:16 +0300

    Home - UsersAdmin added

---
+++ src/Admin/UsersAdmin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class UsersAdmin extends AbstractAdmin
+{
+    protected function configureListFields(ListMapper $list)
+    {
+        $list
+            ->addIdentifier('id')
+            ->addIdentifier('firstName')
+            ->addIdentifier('lastName')
+            ->addIdentifier('email')
+            ->addIdentifier('address');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+            ->add('id')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('address');
+    }
+
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form
+        ->add('id')
+        ->add('firstName')
+        ->add('lastName')
+        ->add('email')
+        ->add('address');
+    }
+}
