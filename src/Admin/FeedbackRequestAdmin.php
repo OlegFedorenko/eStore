96b58f39ee93a6150cef50728c008ecbd2072d6b
OlegFedorenko <oefrdorenko87@gmail.com>
2018-09-23 14:37:30 +0300

    Home - FeedbackRequestAdmin added

---
+++ src/Admin/FeedbackRequestAdmin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class FeedbackRequestAdmin extends AbstractAdmin
+{
+    protected function configureListFields(ListMapper $list)
+    {
+        $list
+            ->addIdentifier('name')
+            ->addIdentifier('email')
+            ->addIdentifier('message');
+
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+            ->add('name')
+            ->add('email')
+            ->add('message');
+    }
+
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form
+            ->add('name')
+            ->add('email')
+            ->add('message');
+    }
+
+
+}
